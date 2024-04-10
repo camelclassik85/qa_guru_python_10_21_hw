bc7cbeaa1c6ad3129d888659dc563359b9475697
Alexander_Dulalaev <camel1985@mail.ru>
2024-04-10 10:36:40 +0300

    upd example

--- .env.example.py
+++ .env.example.py
@@ -1,4 +1,4 @@
-USER_NAME = "bsuser_YoRgWQ"
-ACCESS_KEY = "UT3ozKXA2JvVAqqSEaSd"
+USER_NAME = "bsuser_YoRgWQ1"
+ACCESS_KEY = "UT3ozKXA2JvVAqqSEaSd1"
 BASE_URL = "http://hub.browserstack.com/wd/hub"
 TIMEOUT = 10
